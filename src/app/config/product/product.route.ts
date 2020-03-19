be2fbfde7bd4d79235532426b8e1d9d412c1f7b7
mohamedali.bouzrati <mohamedalibouzrati@gmail.com>
2020-03-19 13:47:04 +0100

    Ajout de ProductResolve

--- src/app/config/product/product.route.ts
+++ src/app/config/product/product.route.ts
@@ -1,7 +1,43 @@
-import { Routes } from '@angular/router';
-import { ProductComponent } from './product.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
+import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot, Routes} from '@angular/router';
+import {EMPTY, Observable, of} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {flatMap} from 'rxjs/operators';
+import {HttpResponse} from '@angular/common/http';
+
+import {ProductComponent} from './product.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {ProductEditComponent} from './product-edit/product-edit.component';
+import {ProductModel} from '../../shared/product.model';
+import {ProductService} from './product.service';
+import {ProductStorageService} from './product-storage.service';
+import {QuantityModel} from '../../shared/quantity.model';
+
+@Injectable({providedIn: 'root'})
+export class ProductResolve implements Resolve<ProductModel> {
+  constructor(private service: ProductService, private productStorageService: ProductStorageService, private router: Router) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ProductModel> | Promise<ProductModel> | ProductModel {
+    const id = route.params['id'];
+    if (id) {
+      return this.productStorageService.getProductById(id).pipe(
+        flatMap((product: HttpResponse<ProductModel>) => {
+          if (product.body) {
+            return of(product.body);
+          } else {
+            this.router.navigate(['404']);
+            return EMPTY;
+          }
+        })
+      );
+    }
+    const product = new ProductModel();
+    product.quantity = new QuantityModel();
+
+    return of(product);
+  }
+
+}
 
 export const productRoute: Routes = [
   {
