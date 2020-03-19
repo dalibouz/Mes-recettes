b7e67a9db0cb98412e2c2eeb1782a6d439579bc6
mohamedali.bouzrati <mohamedalibouzrati@gmail.com>
2020-03-19 17:22:40 +0100

    Récupération des données depuis le serveur dans StockListComponent

--- src/app/mon-stock/stock-list/stock-list.component.ts
+++ src/app/mon-stock/stock-list/stock-list.component.ts
@@ -1,38 +1,33 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
 
 import { ProductInStockModel } from '../../shared/product-in-stock.model';
 import { MonStockService } from '../mon-stock.service';
+import { MonStockStorageService } from '../mon-stock-storage.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-stock-list',
   templateUrl: './stock-list.component.html'
 })
-export class StockListComponent implements OnInit, OnDestroy {
+export class StockListComponent implements OnInit {
   myStock: ProductInStockModel[];
-  subscription: Subscription;
 
   constructor(private monStockService: MonStockService,
+              private monStockStorageService: MonStockStorageService,
               private router: Router,
               private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.subscription = this.monStockService.monStockChanged
-      .subscribe(
-        (monStock: ProductInStockModel[]) => {
-          this.myStock = monStock;
-        }
-      );
-    this.myStock = this.monStockService.getMonStock();
+    this.monStockStorageService.getProductsInMyStock().subscribe(
+      (res: HttpResponse<ProductInStockModel[]>) => {
+        this.myStock = res.body;
+      }
+    );
   }
 
   onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route});
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 }
