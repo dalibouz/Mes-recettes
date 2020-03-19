2f0d963bd2631ee9e897927dc51a66259c69ac15
mohamedali.bouzrati <mohamedalibouzrati@gmail.com>
2020-03-19 16:17:40 +0100

    Résoudre bug quantity-to-string pipe

--- src/app/shared/quantity-to-string.pipe.ts
+++ src/app/shared/quantity-to-string.pipe.ts
@@ -4,9 +4,13 @@ import { QuantityModel } from './quantity.model';
 @Pipe({ name: 'quantityToString' })
 export class QuantityToStringPipe implements PipeTransform {
   transform(quantity: QuantityModel): Object {
+    let quantityString = '';
     if (quantity) {
-      return quantity.amount + ' ' + quantity.unit.symbol;
+      quantityString += quantity.amount;
+      if (quantity.unit) {
+        quantityString += quantity.unit.symbol;
+      }
     }
-    return '';
+    return quantityString;
   }
 }
